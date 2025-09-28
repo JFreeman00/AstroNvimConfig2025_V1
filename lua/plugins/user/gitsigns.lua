0ecf71d7caae972f71a50d97133ddd9a9853525f
YungFreeman <jakobblomdahl@hotmail.com>
2025-09-28 02:18:11 +0200

    added gitsigns

---
+++ lua/plugins/user/gitsigns.lua
@@ -0,0 +1,16 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  opts = {
+    signs = {
+      add = { text = "┃", texthl = "GitSignsAdd" },
+      change = { text = "┃", texthl = "GitSignsChange" },
+      delete = { text = "_", texthl = "GitSignsDelete" },
+      topdelete = { text = "‾", texthl = "GitSignsDelete" },
+      changedelete = { text = "~", texthl = "GitSignsChange" },
+      untracked = { text = "┆", texthl = "GitSignsAdd" },
+    },
+    numhl = false,
+    linehl = false,
+    word_diff = false,
+  },
+}
